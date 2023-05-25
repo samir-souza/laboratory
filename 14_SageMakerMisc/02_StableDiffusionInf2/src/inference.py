e3b6c0e920d6b0d7cee77fecb31164cf20912982
Samir Araujo <samir.araujo@gmail.com>
2023-05-25 21:40:09 +0000

    - custom params & better documentation

--- 14_SageMakerMisc/02_StableDiffusionInf2/src/inference.py
+++ 14_SageMakerMisc/02_StableDiffusionInf2/src/inference.py
@@ -6,6 +6,7 @@ import os
 os.environ['NEURON_RT_NUM_CORES'] = '2'
 import io
 import time
+import json
 import torch
 import numpy as np
 import torch_neuronx
@@ -17,9 +18,6 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
 model_id = "stabilityai/stable-diffusion-2-1-base"
 dtype = DTYPE_REPLACE
-# If you need lower latency, change it to 25. 
-# But the quality of the predictions will not be the same.
-num_inference_steps=50
 
 def model_fn(model_dir, context=None):
     global model_id, dtype
@@ -48,13 +46,24 @@ def model_fn(model_dir, context=None):
     return pipe
 
 def input_fn(request_body, request_content_type, context=None):
-    if request_content_type != 'plain/text':
-        raise Exception(f"Unsupported mime type: {request_content_type}. Supported: plain/text")
-    return request_body.decode('utf-8')
+    if request_content_type == 'application/json':
+        req = json.loads(request_body)
+        prompt = req.get('prompt')
+        num_inference_steps = req.get('num_inference_steps', 50)
+        guidance_scale = req.get('guidance_scale', 7.5)
+        if prompt is None or type(prompt) != str or len(prompt) < 5:
+            raise("Invalid prompt. It needs to be a string > 5")
+        if type(num_inference_steps) != int:
+            raise("Invalid num_inference_steps. Expected int. default = 50")
+        if type(guidance_scale) != float:
+            raise("Invalid guidance_scale. Expected float. default = 7.5")
+        return prompt,num_inference_steps,guidance_scale
+    else:
+        raise Exception(f"Unsupported mime type: {request_content_type}. Supported: application/json")
 
-def predict_fn(input_text, model, context=None):
-    global num_inference_steps
-    return model(input_text, num_inference_steps=num_inference_steps).images[0]    
+def predict_fn(input_req, model, context=None):
+    prompt,num_inference_steps,guidance_scale = input_req
+    return model(prompt, guidance_scale=guidance_scale, num_inference_steps=num_inference_steps).images[0]    
 
 def output_fn(image, accept, context=None):
     if accept!='image/jpeg':
